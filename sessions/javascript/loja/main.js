dbe86cbe0389f2a0cd57d92eca7358e4e1581d04
Rinashala <rinashalaa777@gmail.com>
2024-06-02 23:13:15 +0200

    detyra

---
+++ sessions/javascript/loja/main.js
@@ -0,0 +1,89 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const bgGame = new Image();
+bgGame.src = 'images/background.png';
+
+let bgReady = false;
+let timer = 15;
+
+bgGame.onload = function () {
+    bgReady = true;
+}
+
+let catObj = {};
+catObj.x = 0;
+catObj.y = 40;
+catObj.speed = 5;
+
+let catReady = false;
+const catImg = new Image();
+catImg.src = 'images/cat.png';
+catImg.onload = function () {
+    catReady = true;
+}
+
+let mouseObj = {};
+mouseObj.width = 52;
+mouseObj.height = 54;
+
+mouseObj.x = Math.floor(Math.random() * 460);
+mouseObj.y = Math.floor(Math.random() * 426);
+
+let mouseReady = false;
+const mouseImg = new Image();
+mouseImg.src = 'images/mouse.png';
+mouseImg.onload = function () {
+    mouseReady = true;
+}
+
+function render() {
+    if (bgReady) {
+        ctx.drawImage(bgGame, 0, 0);
+    }
+    if (catReady) {
+        ctx.drawImage(catImg, catObj.x, catObj.y)
+    }
+    if (mouseReady) {
+        ctx.drawImage(mouseImg, mouseObj.x, mouseObj.y)
+    }
+    if (catObj.x > 500) {
+        catObj.x = -50;
+    }
+    if (catObj.x < -51) {
+        catObj.x = 499;
+    }
+    if (catObj.y > 460) {
+        catObj.y = -50;
+    }
+    if (catObj.y < 40) {
+        catObj.y = 41;
+    }
+    ctx.font = "20px Georgia";
+    ctx.fillText("points : 0", 10, 25);
+    ctx.fillStyle = 'white';
+
+    ctx.fillText("timer : " + timer, 400, 25);
+
+};
+
+function time() {
+    timer--;
+}
+addEventListener('keydown', function (e) {
+    console.log(e.key);
+    if (e.key == 'ArrowRight') {
+        catObj.x += catObj.speed
+    }
+    if (e.key == 'ArrowLeft') {
+        catObj.x -= catObj.speed
+    }
+    if (e.key == 'ArrowDown') {
+        catObj.y += catObj.speed
+    }
+    if (e.key == 'ArrowUp') {
+        catObj.y -= catObj.speed
+    }
+});
+
+setInterval(render, 1);
+setInterval(time, 1000);
