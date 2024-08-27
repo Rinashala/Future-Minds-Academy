12883d9573adf5f0d43d17d38c7e800bb75d12b8
Rinashala <rinashalaa777@gmail.com>
2024-08-27 13:43:19 +0200

    2.0

--- msim/js/JAVASCRIPT HANDS-ON]/assets/js/notifications.js
+++ msim/js/JAVASCRIPT HANDS-ON]/assets/js/notifications.js
@@ -1,27 +1,21 @@
 let originalTitle = document.title;
 let originalFavicon = document.querySelector('link[rel="icon"]').href;
-
 let interval;
-
 function startNotification(text) {
     if (interval) {
         clearInterval(interval);
     }
-
     interval = setInterval(() => {
-        document.title = document.title === text ? originalTitle : text;
 
-        let favicon = document.querySelector('link[rel="icon"]');
-        favicon.href = favicon.href === originalFavicon ? "assets/img/4.webp?v=" + new Date().getTime() : originalFavicon;
     }, 1000);
 }
-
+interval = setInterval(() => { startNotification(text) }, 1000);
 function endNotification() {
     if (interval) {
         clearInterval(interval);
     }
-
     document.title = originalTitle;
     let favicon = document.querySelector('link[rel="icon"]');
     favicon.href = originalFavicon;
+
 }
